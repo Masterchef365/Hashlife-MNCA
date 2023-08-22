7324daa9acb50d01a2af6218b79aad9603861a7f
Masterchef365 <duncan.freeman1@gmail.com>
2023-08-21 19:09:36 -0700

    Start thinking about dense sim

--- src/sim.rs
+++ src/sim.rs
@@ -1,7 +1,6 @@
-pub type NodeIdx = usize;
-
 /// Block data, whose size is known by the Kernel
-pub type Block = Box<[NodeIdx]>;
+#[derive(Clone, Debug, Default)]
+pub struct Block(Box<[bool]>);
 
 pub trait Kernel {
     /// Power law size of the basic block. E.g. each block has a width of 2^n, where n = self.order()
@@ -22,3 +21,97 @@ pub enum KernelResult {
     Approximate,
 }
 
+struct Dense {
+    blocks: Vec<Block>,
+    width: usize,
+    kernel: Box<dyn Kernel>,
+}
+
+impl Dense {
+    pub fn new(kernel: Box<dyn Kernel>, width: usize, height: usize) -> Self {
+        Self {
+            blocks: vec![Block::zero(&*kernel); width * height],
+            width,
+            kernel,
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn height(&self) -> usize {
+        assert_eq!(
+            self.blocks.len() % self.width(),
+            0,
+            "Invalid number of blocks..."
+        );
+        self.blocks.len() / self.width()
+    }
+
+    pub fn index(&self, index: (i32, i32)) -> Option<usize> {
+        let (x, y) = index;
+        let in_bounds = x >= 0 && y >= 0 && x < self.width() as i32 && y < self.height() as i32;
+        in_bounds.then(|| x as usize + y as usize * self.height())
+    }
+
+    pub fn get(&self, index: (i32, i32)) -> Option<&Block> {
+        self.index(index).and_then(|i| self.blocks.get(i))
+    }
+
+    pub fn get_mut(&mut self, index: (i32, i32)) -> Option<&mut Block> {
+        self.index(index).and_then(|i| self.blocks.get_mut(i))
+    }
+
+    pub fn get_pixel(&self, pixel: (i32, i32)) -> bool {
+        let (x, y) = pixel;
+        let x_block = x / self.width() as i32;
+        let y_block = x / self.height() as i32;
+        let block = &self.get((x_block, y_block)).unwrap();
+
+        block.get(&*self.kernel, (x, y))
+    }
+}
+
+impl Block {
+    pub fn zero(ker: &dyn Kernel) -> Self {
+        Self::new(ker, vec![false; Self::width(ker).pow(2)])
+    }
+
+    pub fn new(ker: &dyn Kernel, data: Vec<bool>) -> Self {
+        let expected_len = Self::width(ker).pow(2);
+        assert_eq!(expected_len, data.len());
+        Self(data.into_boxed_slice())
+    }
+
+    pub fn width(ker: &dyn Kernel) -> usize {
+        1 << ker.order()
+    }
+
+    pub fn data_mut(Block(data): &mut Self) -> &mut [bool] {
+        data
+    }
+
+    pub fn data(Block(data): &Self) -> &[bool] {
+        data
+    }
+
+    /// Get a subpixel within a block by x and y coordinates
+    /// X and Y wrap around block size, which is in units of 2^n
+    pub fn index(ker: &dyn Kernel, xy: (i32, i32)) -> usize {
+        let block_width = Self::width(ker) as i32;
+        let (x, y) = xy;
+        let (x, y) = (x % block_width, y % block_width);
+        (x + block_width * y) as usize
+    }
+
+    pub fn get(&self, ker: &dyn Kernel, xy: (i32, i32)) -> bool {
+        let Self(data) = self;
+        data[Self::index(ker, xy)]
+    }
+
+    pub fn set(&mut self, ker: &dyn Kernel, xy: (i32, i32), val: bool) {
+        let Self(data) = self;
+        data[Self::index(ker, xy)] = val;
+    }
+}
