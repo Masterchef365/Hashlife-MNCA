f7fc8c783e74569ed9dc1d978c421cae765b6534
Masterchef365 <duncan.freeman1@gmail.com>
2023-08-21 19:21:34 -0700

    Whole lotta hoopla

--- src/sim.rs
+++ src/sim.rs
@@ -25,6 +25,7 @@ struct Dense {
     blocks: Vec<Block>,
     width: usize,
     kernel: Box<dyn Kernel>,
+    offset_is_needed: bool,
 }
 
 impl Dense {
@@ -33,41 +34,63 @@ impl Dense {
             blocks: vec![Block::zero(&*kernel); width * height],
             width,
             kernel,
+            offset_is_needed: false,
         }
     }
 
-    pub fn width(&self) -> usize {
+    pub fn step(&mut self) {
+    }
+
+    /// Returns (width, height) in pixels
+    pub fn pixel_dims(&self) -> (usize, usize) {
+        let w = Block::width(&*self.kernel);
+        (
+            self.width_blocks() * w,
+            self.height_blocks() * w,
+        )
+    }
+
+    pub fn width_blocks(&self) -> usize {
         self.width
     }
 
-    pub fn height(&self) -> usize {
+    pub fn height_blocks(&self) -> usize {
         assert_eq!(
-            self.blocks.len() % self.width(),
+            self.blocks.len() % self.width_blocks(),
             0,
             "Invalid number of blocks..."
         );
-        self.blocks.len() / self.width()
+        self.blocks.len() / self.width_blocks()
     }
 
     pub fn index(&self, index: (i32, i32)) -> Option<usize> {
-        let (x, y) = index;
-        let in_bounds = x >= 0 && y >= 0 && x < self.width() as i32 && y < self.height() as i32;
-        in_bounds.then(|| x as usize + y as usize * self.height())
+        let (mut x, mut y) = index;
+
+        // Every other frame, the blocks are in an alternate configuration where the result of the
+        // last frame is offset by half
+        if self.offset_is_needed {
+            let w = Block::width(&*self.kernel) / 2;
+            x += w as i32;
+            y += w as i32;
+        }
+
+        let in_bounds = x >= 0 && y >= 0 && x < self.width_blocks() as i32 && y < self.height_blocks() as i32;
+        in_bounds.then(|| x as usize + y as usize * self.height_blocks())
     }
 
-    pub fn get(&self, index: (i32, i32)) -> Option<&Block> {
+    pub fn get_block(&self, index: (i32, i32)) -> Option<&Block> {
         self.index(index).and_then(|i| self.blocks.get(i))
     }
 
-    pub fn get_mut(&mut self, index: (i32, i32)) -> Option<&mut Block> {
+    pub fn get_mut_block(&mut self, index: (i32, i32)) -> Option<&mut Block> {
         self.index(index).and_then(|i| self.blocks.get_mut(i))
     }
 
     pub fn get_pixel(&self, pixel: (i32, i32)) -> bool {
         let (x, y) = pixel;
-        let x_block = x / self.width() as i32;
-        let y_block = x / self.height() as i32;
-        let block = &self.get((x_block, y_block)).unwrap();
+        let x_block = x / self.width_blocks() as i32;
+        let y_block = y / self.height_blocks() as i32;
+        let block = &self.get_block((x_block, y_block)).unwrap();
 
         block.get(&*self.kernel, (x, y))
     }
